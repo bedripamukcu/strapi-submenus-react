1ffe2af6cb0b2bad28a8c80f6aab5f30a375f5cc
bedripamukcu <niyazibedripamukcu@gmail.com>
2023-06-23 08:54:57 +0300

    first commit

---
+++ starter/src/Hero.jsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+  return (
+    <div className="hero-container">
+        <div className="hero-center">
+            <h1>Manage Any Content <br/> Anywhere
+            </h1>
+            <p>
+                Strapi is the loading open-source headless CMS. İt's 100% Javascrip and fully customizable
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
